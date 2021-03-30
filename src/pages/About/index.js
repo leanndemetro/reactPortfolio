8745470e95d21cba0acda9cad643291b3ecc2a71
Alyssa DeMetro <leanndemetro@gmail.com>
2021-03-30 19:22:56 -0400

    small changes to about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -20,40 +20,17 @@ function About() {
   <hr></hr>
   <Row>
   <Col sm={4}>
-  <div className="aboutCardRight">
-        <img alt="bootcampCertificate" className="meImg" src="https://i.imgur.com/dry8RG7.jpg?1"></img>
-  </div>
   <div className="aboutCardLeft">
         <p className="aboutText">My name is Alyssa DeMetro - I'm an Orlando Native, and a graduate of the University Of Central Florida Full-Stack Coding Bootcamp.</p>
+        </div>
+  <div className="aboutCardLeft">
+        <p className="aboutText">I have a fundamental working knowledge of HTML, CSS, JavaScript, jQuery, Node.js, Express.js, MySQL, MongoDB, Mongoose, React.js, and more. I have experience working with diverse teams or independently to meet demanding deadlines. I am confident and capable of building fully functional MERN stack web applications from start to finish. I am passionate about continuing my education and learning new technologies, and I have the patience and research skills necessary to resolve issues independently.
+            I am actively seeking intership, volunteer, or employment opportunities.</p>
         </div>
       </Col>
     <Col sm={8}>
       <div className="aboutCardRight">
         <a target="_blank" rel="noreferrer" href="https://api.badgr.io/public/assertions/tDt35IJZQT-VVtAvMjxGfA?identity__email=leanndemetro%40gmail.com"><img alt="certificate" className="certImg" src="https://i.imgur.com/ZEsoyI7.jpg"></img></a>
-      </div>
-      <br></br>
-      <br></br></Col>
-  </Row>
-  <Row>
-    <Col>
-    <hr></hr>
-    </Col>
-  </Row>
-  <Row>
-  <br></br></Row>
-  <Row>
-    <Col sm={8}>
-    <a className="resLink" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/alyssa-de-metro-59abba1b0/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAADFMxcYB0XJ1lHN-GVVtohMQbcYjdf0W4pc,1614205808207)/">
-  <div className="aboutCardRight">
-  <img className="resImg" alt="linkedIn" src="https://i.imgur.com/JJbbBz3.jpg"></img>
-  </div>
-  </a>
-    </Col>
-     <Col sm={4}>
-     <div className="aboutCardLeft">
-          <p className="aboutTextTwo"> I have a fundamental working knowledge of HTML, CSS, JavaScript, jQuery, Node.js, Express.js, MySQL, MongoDB, Mongoose, React.js, and more. I have experience working with diverse teams or independently to meet demanding deadlines. I am confident and capable of building fully functional MERN stack web applications from start to finish. I am passionate about continuing my education and learning new technologies, and I have the patience and research skills necessary to resolve issues independently.
-            I am actively seeking intership, volunteer, or employment opportunities.
-          </p>
       </div>
       </Col>
   </Row>
