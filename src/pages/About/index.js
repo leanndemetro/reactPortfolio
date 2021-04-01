aefdd89b124702ba79b1c9a0c593742cbaf05f7d
Alyssa DeMetro <leanndemetro@gmail.com>
2021-03-31 20:53:53 -0400

    about page edits

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css"
 import { Container, Row, Col } from "react-bootstrap";
+import { FaLongArrowAltRight } from 'react-icons/fa';
 
 
 
@@ -18,13 +19,13 @@ function About() {
       </Col>
   </Row>
   <hr></hr>
-  <Row>
+  <Row className="aboutDiv">
   <Col sm={4}>
   <div className="aboutCardLeft">
-        <p className="aboutText">My name is Alyssa DeMetro - I'm an Orlando Native, and a graduate of the University Of Central Florida Full-Stack Coding Bootcamp.</p>
-        </div>
-  <div className="aboutCardLeft">
-        <p className="aboutText">I have a fundamental working knowledge of HTML, CSS, JavaScript, jQuery, Node.js, Express.js, MySQL, MongoDB, Mongoose, React.js, and more. I have experience working with diverse teams or independently to meet demanding deadlines. I am confident and capable of building fully functional MERN stack web applications from start to finish. I am passionate about continuing my education and learning new technologies, and I have the patience and research skills necessary to resolve issues independently.
+        <p className="aboutText"><div className="aDemetro">Alyssa DeMetro</div> 
+        <br></br>
+        An Orlando Native, and a graduate of the University Of Central Florida Full-Stack Coding Bootcamp.</p>
+        <p>I have a fundamental working knowledge of HTML, CSS, JavaScript, jQuery, Node.js, Express.js, MySQL, MongoDB, Mongoose, React.js, and more. I have experience working with diverse teams or independently to meet demanding deadlines. I am confident and capable of building fully functional MERN stack web applications from start to finish. I am passionate about continuing my education and learning new technologies, and I have the patience and research skills necessary to resolve issues independently.
             I am actively seeking intership, volunteer, or employment opportunities.</p>
         </div>
       </Col>
@@ -32,9 +33,8 @@ function About() {
       <div className="aboutCardRight">
         <a target="_blank" rel="noreferrer" href="https://api.badgr.io/public/assertions/tDt35IJZQT-VVtAvMjxGfA?identity__email=leanndemetro%40gmail.com"><img alt="certificate" className="certImg" src="https://i.imgur.com/ZEsoyI7.jpg"></img></a>
       </div>
-      </Col>
+     </Col>
   </Row>
-  <br></br>
   <hr></hr>
   <br></br>
   </Container>
