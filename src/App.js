64ae044051766643f17fffd088d856f1581adeff
Alyssa DeMetro <leanndemetro@gmail.com>
2021-04-14 18:51:26 -0400

    dashboard page complete with footer, possible updates to footer still needed

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ function App() {
             <Portfolio />
           </Route>
           </Switch>
+          <Footer />
       </div>
     </Router>
   );
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,25 +4,32 @@ import { Container, Row, Col } from "react-bootstrap";
 
 function Dashboard() {
   return (
-  <div>
-    <Container>
-      <div className="dashDiv">
-    <Row>
-      <Col>ALYSSA DEMETRO
+    <div>
+      <Container>
+        <div className="dashDiv">
+          <Row>
+            <Col>ALYSSA DEMETRO
       <hr></hr></Col>
-    </Row>
-    <Row>
-      <Col><p className="dashRowTwo">MERN STACK WEB DEVELOPER</p></Col>
-    </Row>
-    <Row>
-      <Col><a href="/About"><button className="learnButton"><p className="learnText">LEARN MORE</p></button></a></Col>
-    </Row>
-    </div>
-    </Container>
+          </Row>
+          <Row>
+            <Col><p className="dashRowTwo">MERN STACK WEB DEVELOPER</p></Col>
+          </Row>
+          <Row>
+            <Col><a href="/About"><button className="learnButton"><p className="learnText">LEARN MORE</p></button></a></Col>
+          </Row>
+        </div>
+      </Container>
+      <br></br>
+        <br></br>
+        <br></br>
+      <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
     </div>
 
-   
-  
+
+
 
   );
 }
